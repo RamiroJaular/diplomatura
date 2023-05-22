103305222784e84412731c5e9ad001cbfe109890
ramiro jaular <jaular7@gmail.com>
2023-05-22 11:55:41 -0300

    clase 19

---
+++ clase 19/ejercicio2/js/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+    $window = $($window);
+    $('#intro').height(window.height());
+    $window.scroll(function () {
+        $('div.background').each(function (index, item) {
+            var $scroll = $(item);
+            var yPos = -($window.scrollTop() / $scroll.data('speed'));
+            var coords = '50%' + yPos + 'px';
+            $scroll.css({ backgroundPosition: coords });
+        })
+
+
+    })// cierra la segunda
+
+
+}) //fin
